07df9a320fc114fb25e83e9716b7b47ce0b3dd84
dindatisi <dindatcalista@veritrans.co.id>
2018-05-13 21:19:29 +0100

    update poi recommender columns & use count vectorizer instead of tfidf

--- poi_recommender.py
+++ poi_recommender.py
@@ -3,10 +3,7 @@ import recommender_engine as re
 
 def main():	
 	df = pd.read_csv('data/cleaned_poi.csv')
-	print('\n calculate tfidf')
-	tfidf_matrix = re.get_tfidf(df.rev_cat_soup)
-	print('matrix shape: ', tfidf_matrix.shape)
-	cosine_sim = re.get_cosine_sim(tfidf_matrix)
-	df_result = re.get_recommendation(df,'Tate Modern', 'place_name',cosine_sim)
+	cosine_sim = re.get_count_similarity(df, df.rev_cat_soup.astype(str))
+	df_result = re.get_recommendation(df,'Camden Arts Center', 'place_name',cosine_sim)
 	print(df_result[['place_name','relative_score','similarity_score','popularity_index','categories','reviews_combined']])
 main()
